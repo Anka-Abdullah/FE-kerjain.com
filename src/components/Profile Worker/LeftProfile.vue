5fb6fc97aece142fdf3ce44bfedd3f66286e9e60
ivanrozak <ivanrozack@gmail.com>
2021-02-09 20:18:55 +0700

    add delete porto, experience and change password

--- src/components/Profile Worker/LeftProfile.vue
+++ src/components/Profile Worker/LeftProfile.vue
@@ -51,6 +51,44 @@
         Batal
       </button></b-row
     >
+    <b-row>
+      <button
+        @click="$bvModal.show('bv-modal-example')"
+        class="button btn-secondary w-100 mx-3 mb-4"
+      >
+        Ubah password
+      </button></b-row
+    >
+    <b-modal id="bv-modal-example" hide-footer>
+      <template #modal-title> Ubah password </template>
+      <div class="d-block">
+        <h6>Password baru</h6>
+        <b-form-input
+          type="password"
+          v-model="form.newPassword"
+          placeholder="Masukkan password baru"
+          required
+        ></b-form-input>
+        <br />
+        <h6>Konfirmasi password baru</h6>
+        <b-form-input
+          type="password"
+          v-model="form.confirmPassword"
+          placeholder="Konfirmasi password baru"
+          required
+        ></b-form-input>
+      </div>
+      <b-button
+        class="mt-3"
+        variant="outline-primary"
+        block
+        @click="changePass()"
+        >Ubah password</b-button
+      >
+      <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')"
+        >Batal</b-button
+      >
+    </b-modal>
   </div>
 </template>
 
@@ -62,14 +100,22 @@ export default {
   data() {
     return {
       image: '',
-      url: process.env.VUE_APP_URL
+      url: process.env.VUE_APP_URL,
+      form: {
+        newPassword: '',
+        confirmPassword: ''
+      }
     }
   },
   computed: {
     ...mapGetters({ data: 'setUserId' })
   },
   methods: {
-    ...mapActions(['updateProfileUsers', 'UpdateImageUsers']),
+    ...mapActions([
+      'updateProfileUsers',
+      'UpdateImageUsers',
+      'changePasswords'
+    ]),
     updateProfile() {
       this.updateProfileUsers(this.data)
         .then(result => {
@@ -98,6 +144,15 @@ export default {
     },
     toHome() {
       this.$router.push('/')
+    },
+    changePass() {
+      this.changePasswords(this.form)
+        .then(result => {
+          this.successAlert(result.data.msg)
+        })
+        .catch(err => {
+          this.errorAlert(err.data.msg)
+        })
     }
   }
 }
